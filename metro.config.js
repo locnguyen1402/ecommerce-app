16ea434d7689e7c85b12bf771b55c13cf9371fe2
Loc Nguyen <loc.nguyen@vklink.vn>
2025-06-18 22:53:55 +0700

    remove

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-const config = getDefaultConfig(__dirname);
-
-// Add CSS support for web
-config.resolver.assetExts.push("css");
-
-module.exports = withNativeWind(config, { input: "./global.css" });
